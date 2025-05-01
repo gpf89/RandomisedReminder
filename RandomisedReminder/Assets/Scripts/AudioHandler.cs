28a4c72188c5fc74eb2211f4f93053e13d7023eb
Gergely Ferenczi <gergely@mxt.co.uk>
2025-05-01 19:06:35 +0100

    Add basic functionality
    Create some buttons for Playing, Pausing and Changing audio clips. Most
    of this will not be needed but they are development tools for while I
    figure out what I need.

--- RandomisedReminder/Assets/Scripts/AudioHandler.cs
+++ RandomisedReminder/Assets/Scripts/AudioHandler.cs
@@ -12,9 +12,23 @@ class AudioHandlerEditor : Editor
         var audioHandler = (AudioHandler)target;
         if (audioHandler == null) return;
 
+        EditorGUILayout.BeginHorizontal();
         if (GUILayout.Button("Play clip"))
         {
             audioHandler.PlayClip();
+        }
+        if (GUILayout.Button("Pause clip"))
+        {
+            audioHandler.PauseClip();
+        }
+        if (GUILayout.Button("Stop clip"))
+        {
+            audioHandler.StopClip();
+        }
+        EditorGUILayout.EndHorizontal();
+        if (GUILayout.Button("Next clip"))
+        {
+            audioHandler.ChangeClip();
         }
         //base.OnInspectorGUI();
         DrawDefaultInspector();
@@ -25,10 +39,30 @@ class AudioHandlerEditor : Editor
 
 public class AudioHandler : MonoBehaviour
 {
-    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioSource _audioSource;
+    [Header("Clips")]
+    [SerializeField] AudioClip _start;
+    [SerializeField] List<AudioClip> _reminders;
+
+    int _counter = 0;
 
     public void PlayClip()
     {
-        audioSource.Play();
+        _audioSource.Play();
+    }
+
+    public void PauseClip()
+    {
+        _audioSource.Pause();
+    }
+    public void StopClip()
+    {
+        _audioSource.Stop();
+    }
+
+    public void ChangeClip()
+    {
+        _counter++;
+        _audioSource.clip = _reminders[_counter % _reminders.Count];
     }
 }
