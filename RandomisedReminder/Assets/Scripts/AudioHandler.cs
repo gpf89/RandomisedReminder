6a42e09bd992f37ab6b8a0cd3cdcb69629d233c4
Gergely Ferenczi <gergely@mxt.co.uk>
2025-06-23 19:51:43 +0100

    Add slider to control duration
    Add a slider UI element and display the time that is set by it. Restrict
    the range to between 5 and 60 minutes. During countdown set the slider
    to inactive. Move the display to above the slider.

--- RandomisedReminder/Assets/Scripts/AudioHandler.cs
+++ RandomisedReminder/Assets/Scripts/AudioHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 #if UNITY_EDITOR
 using UnityEditor;
@@ -40,6 +41,10 @@ class AudioHandlerEditor : Editor
         {
             audioHandler.BeginCountdown();
         }
+        if (GUILayout.Button("EndCOuntdown"))
+        {
+            audioHandler.EndCountdown();
+        }
         EditorGUILayout.TextField(audioHandler.FormattedTime(audioHandler.RemainingTime));
         //base.OnInspectorGUI();
         DrawDefaultInspector();
@@ -58,8 +63,8 @@ public class AudioHandler : MonoBehaviour
     }
 
     [Header("User Controlls")]
-    [Range(0,60)]
-    [SerializeField] private int _duration = 15;
+    [Range(5,60)]
+    [SerializeField] private int _duration;
     [SerializeField] private Density _density;
     [Header("Clips")]
     [SerializeField] private AudioClip _startClip;
@@ -67,6 +72,7 @@ public class AudioHandler : MonoBehaviour
     [Header("Config")]
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private TextMeshProUGUI _displayTime;
+    [SerializeField] private Slider _durationSetter;
     
     public float RemainingTime { get => _remainingTime;  }
     private float _remainingTime;
@@ -76,20 +82,20 @@ public class AudioHandler : MonoBehaviour
     private const float MINS_TO_SECS = 60f;
     private const float FRACTIONAL_VARIATION = 0.2f;
 
-    // this will vary later
-    private int _clipCount = 4;
-
     //For Debug purposes only
     private int _clipCounter = 0;
 
     public void Start()
     {
+        _durationSetter.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
         _remainingTime = _duration * MINS_TO_SECS;
+        _displayTime.text = FormattedTime(_remainingTime);
     }
 
     public void Update()
     {
         if (_isCountingDown == false) return;
+        _durationSetter.gameObject.SetActive(false);
         if (_remainingTime > 0f)
         {
             _remainingTime -= Time.deltaTime;
@@ -104,6 +110,7 @@ public class AudioHandler : MonoBehaviour
         else
         {
             _isCountingDown = false;
+            _durationSetter.gameObject.SetActive(true);
         }
 
 
@@ -157,12 +164,29 @@ public class AudioHandler : MonoBehaviour
         Debug.Log($"Clip timer reset, time to next clip: {FormattedTime(_timeToNextClip)}, intervalDeviation: {intervalDeviation}");
     }
 
+    private void ValueChangeCheck()
+    {
+        _duration = (int)_durationSetter.value + 5;
+        _remainingTime = _duration * MINS_TO_SECS;
+        _displayTime.text = FormattedTime(_remainingTime);
+        Debug.Log($"Countdown timer set to " + _displayTime.text);
+    }
+
     public void BeginCountdown()
     {
         Initialise();
         _isCountingDown = true;
         _audioSource.clip = _startClip;
         _audioSource.Play();
+    }
+
+    public void EndCountdown()
+    {
+        _isCountingDown = false;
+        _audioSource.Stop();
+        Initialise();
+        _displayTime.text = FormattedTime(_remainingTime);
+        _durationSetter.gameObject.SetActive(true);
     }
 
     private void PlayNextClip()
